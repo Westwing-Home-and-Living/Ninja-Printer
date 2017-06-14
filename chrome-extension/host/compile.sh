a6867014d3b84b730eb8ef7d2708cfa49116e964
Peter Aba <peter@peteraba.com>
2017-06-14 14:42:37 +0200

    Adding unit tests to compile

--- chrome-extension/host/compile.sh
+++ chrome-extension/host/compile.sh
@@ -6,8 +6,20 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Cd into the java host root
 cd "$__dir/../../java"
 
+# Run Java unit tests
+echo "Running: ant test"
+
+exec 5>&1
+test_output=$(ant test | tee /dev/fd/5)
+
+if [[ $test_output != *"BUILD SUCCESSFUL"* ]]; then
+  echo "Java unit tests failed."
+  exit
+fi
+
 # Create the java class files
-echo "Run ant"
+echo ""
+echo "Running: ant"
 ant
 
 # Display results
